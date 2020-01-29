563125d3488e2aece9d1aa9864af357ee4f3ce58
tiktok <mehdi-098@outlook.fr>
2020-01-29 14:39:56 +0100

    new commit

---
+++ Test.java
@@ -0,0 +1,28 @@
+    
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Test
+     */
+    public Test()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+   
+    public static void main(String args[])
+    {
+        Game t=new Game();
+        t.play();
+    }
+}
