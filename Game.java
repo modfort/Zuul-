e4a4435bd692d1cc8d2cb86668bb829163d44907
tiktok <mehdi-098@outlook.fr>
2020-01-31 23:47:45 +0100

    add printinfolocation

--- Game.java
+++ Game.java
@@ -81,17 +81,8 @@ public class Game
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
         System.out.print("Exits: ");
-        if(currentRoom.northExit != null)
-            System.out.print("north ");
-        if(currentRoom.eastExit != null)
-            System.out.print("east ");
-        if(currentRoom.southExit != null)
-            System.out.print("south ");
-        if(currentRoom.westExit != null)
-            System.out.print("west ");
-        System.out.println();
+        printLocationInfo();
     }
 
     /**
@@ -169,7 +160,12 @@ public class Game
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
+            printLocationInfo();
+        }
+    }
+private void printLocationInfo(){
+
+     System.out.println("You are " + currentRoom.getDescription());
             System.out.print("Exits: ");
             if(currentRoom.northExit != null)
                 System.out.print("north ");
@@ -180,9 +176,8 @@ public class Game
             if(currentRoom.westExit != null)
                 System.out.print("west ");
             System.out.println();
-        }
-    }
-
+    
+}
     /** 
      * "Quit" was entered. Check the rest of the command to see
      * whether we really quit the game.
